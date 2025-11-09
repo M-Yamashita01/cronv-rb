b1c4bf38570fbb06d5811c90c46d9366d1cf6c3f
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2025-11-09 21:35:14 +0900

    Adjust to_from_time spec to satisfy RuboCop block length limit

--- spec/cronv_rb/option_spec.rb
+++ spec/cronv_rb/option_spec.rb
@@ -5,28 +5,15 @@ require 'spec_helper'
 RSpec.describe CronvRb::Option do
   describe '#to_from_time' do
     context 'with valid date and time' do
-      it 'should return Time object in UTC' do
+      it 'returns parsed Time object in UTC' do
         now = Time.new(2016, 11, 8, 1, 30, 0, 0)
         option = CronvRb::Option.new_cronv_option(now)
+
         result = option.to_from_time
 
         expect(result).to be_a(Time)
-        expect(result.year).to eq(2016)
-        expect(result.month).to eq(11)
-        expect(result.day).to eq(8)
-        expect(result.hour).to eq(1)
-        expect(result.min).to eq(30)
         expect(result.utc?).to be true
-      end
-
-      it 'should parse date and time correctly' do
-        # Go版のテストケースを参考に
-        now = Time.new(2016, 11, 8, 1, 30, 0, 0)
-        option = CronvRb::Option.new_cronv_option(now)
-        result = option.to_from_time
-
-        expected = Time.new(2016, 11, 8, 1, 30, 0, 0).utc
-        expect(result).to eq(expected)
+        expect(result).to eq(Time.utc(2016, 11, 8, 1, 30, 0))
       end
     end
   end
