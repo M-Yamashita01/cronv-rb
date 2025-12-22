d852eabcf503171c98b5ecb23f99bf9eb2fa30b0
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2025-12-23 06:09:56 +0900

    Add Extra class and enhance Crontab parsing functionality
    - Introduced the Extra class to handle '@reboot' crontab lines.
    - Enhanced the Crontab class with a parse method to create Crontab objects from valid crontab lines.
    - Updated tests to cover new parsing functionality and ensure correct behavior for various crontab inputs.

--- lib/cronv_rb/crontab.rb
+++ lib/cronv_rb/crontab.rb
@@ -1,7 +1,73 @@
 # frozen_string_literal: true
 
 module CronvRb
+  class Extra
+    attr_reader :line, :label, :job
+
+    def initialize(line:, label:, job:)
+      @line = line
+      @label = label
+      @job = job
+    end
+  end
+
   class Crontab
+    attr_reader :line, :schedule, :job
+
+    def initialize(line:, schedule:, job:)
+      @line = line
+      @schedule = schedule
+      @job = job
+    end
+
+    class << self
+      def parse(context = {}, line)
+        parts = line.split(' ')
+        schedule = nil
+        job = []
+
+        if parts[0].start_with?('@')
+          if parts.length < 2
+            # TODO: raise error
+            return nil
+          end
+
+          if parts[0] == '@reboot'
+            return Extra.new(
+              line: line,
+              label: parts[0],
+              job: parts[1..].join(' ')
+            )
+          end
+
+        else
+          if parts.length < 5
+            # TODO: メッセージの改善
+            Logger.error("invalid crontab line: '#{line}'")
+            return nil
+          end
+
+          schedule = Schedule.new(
+            parts[0],
+            parts[1],
+            parts[2],
+            parts[3],
+            parts[4],
+            nil,
+            nil
+          )
+
+          job = parts[5..].join(' ')
+        end
+
+        Crontab.new(
+          line: line,
+          schedule: schedule,
+          job: job
+        )
+      end
+    end
+
     def running_every_minutes?(schedule)
       return true if schedule.minutes == '*' || schedule.minutes == '*/1'
 
--- spec/cronv_rb/crontab_spec.rb
+++ spec/cronv_rb/crontab_spec.rb
@@ -4,11 +4,13 @@ require 'spec_helper'
 
 RSpec.describe CronvRb::Crontab do
   describe '#running_every_minutes?' do
+    let(:crontab) { described_class.new(line: '', schedule: nil, job: nil) }
+
     context 'when the schedule is running every minute using *' do
       let(:schedule) { CronvRb::Schedule.new('*', '*', '*', '*', '*', '*', nil) }
 
       it 'should return true' do
-        expect(described_class.new.running_every_minutes?(schedule)).to be true
+        expect(crontab.running_every_minutes?(schedule)).to be true
       end
     end
 
@@ -16,7 +18,7 @@ RSpec.describe CronvRb::Crontab do
       let(:schedule) { CronvRb::Schedule.new('*/1', '*', '*', '*', '*', '*', nil) }
 
       it 'should return true' do
-        expect(described_class.new.running_every_minutes?(schedule)).to be true
+        expect(crontab.running_every_minutes?(schedule)).to be true
       end
     end
 
@@ -24,7 +26,43 @@ RSpec.describe CronvRb::Crontab do
       let(:schedule) { CronvRb::Schedule.new('0', '0', '1', '1', '1', '2024', nil) }
 
       it 'should return false' do
-        expect(described_class.new.running_every_minutes?(schedule)).to be false
+        expect(crontab.running_every_minutes?(schedule)).to be false
+      end
+    end
+  end
+
+  describe '.parse' do
+    context 'when the line is a valid crontab line' do
+      let(:line) { '0 0 * * * /usr/bin/somecommand' }
+
+      it 'should return a Crontab object' do
+        crontab = described_class.parse(line)
+        expect(crontab).to be_a(CronvRb::Crontab)
+        expect(crontab.line).to eq(line)
+        expect(crontab.schedule.minutes).to eq('0')
+        expect(crontab.schedule.hour).to eq('0')
+        expect(crontab.schedule.day_of_month).to eq('*')
+        expect(crontab.schedule.month).to eq('*')
+        expect(crontab.schedule.day_of_week).to eq('*')
+        expect(crontab.schedule.year).to be_nil
+        # expect(crontab.schedule.schedule_alias).to be_nil
+        expect(crontab.job).to eq('/usr/bin/somecommand')
+      end
+    end
+
+    context 'when the line is an valid crontab line and command with arguments' do
+      let(:line) { '0 0 * * * /usr/bin/somecommand arg1 arg2' }
+
+      it 'should return a Crontab object' do
+        crontab = described_class.parse(line)
+        expect(crontab.schedule.minutes).to eq('0')
+        expect(crontab.schedule.hour).to eq('0')
+        expect(crontab.schedule.day_of_month).to eq('*')
+        expect(crontab.schedule.month).to eq('*')
+        expect(crontab.schedule.day_of_week).to eq('*')
+        expect(crontab.schedule.year).to be_nil
+        # expect(crontab.schedule.schedule_alias).to be_nil
+        expect(crontab.job).to eq('/usr/bin/somecommand arg1 arg2')
       end
     end
   end
