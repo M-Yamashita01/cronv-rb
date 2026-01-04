7cf843c495d09ef0f87ef94b422e5e8f190d8732
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2026-01-04 22:53:08 +0900

    Add add method to Visualizer class for handling cron schedule lines
    - Introduced the `add` method to process cron schedule lines, allowing for the addition of records and extras.
    - Implemented logic to ignore empty lines and comments.
    - Updated the Visualizer class to include `records` and `extras` attributes for storing added entries.
    - Added corresponding tests to validate the functionality of the new method.

--- lib/cronv_rb/visualizer.rb
+++ lib/cronv_rb/visualizer.rb
@@ -5,6 +5,8 @@ require 'time'
 
 module CronvRb
   class Visualizer
+    attr_reader :option, :time_from, :time_to, :duration_minutes, :records, :extras
+
     class << self
       def new_visualizer(option)
         time_from = option.to_from_time
@@ -15,6 +17,19 @@ module CronvRb
       end
     end
 
+    def add(line)
+      trimmed = line.strip
+
+      return [false, nil] if trimmed.empty? || trimmed.start_with?('#')
+
+      record, extra = Record.new_record(line: trimmed, start_time: @time_from, duration_minutes: @duration_minutes)
+
+      @records.push(record) unless record.nil?
+      @extras.push(extra) unless extra.nil?
+
+      [true, nil]
+    end
+
     private
 
     def initialize(option, time_from, time_to, duration_minutes)
@@ -22,6 +37,8 @@ module CronvRb
       @time_from = time_from
       @time_to = time_to
       @duration_minutes = duration_minutes
+      @records = []
+      @extras = []
     end
   end
 end
--- spec/cronv_rb/visualizer_spec.rb
+++ spec/cronv_rb/visualizer_spec.rb
@@ -19,4 +19,42 @@ RSpec.describe CronvRb::Visualizer do
       expect(time_to).to eq(Time.utc(2024, 11, 25, 10, 30, 0))
     end
   end
+
+  describe '#add' do
+    let(:now) { Time.new(2024, 11, 25, 13, 30, 0, '+09:00') }
+
+    context 'when the line is a valid cron schedule' do
+      it 'should add a record' do
+        option = CronvRb::Option.new_cronv_option(now)
+        visualizer = CronvRb::Visualizer.new_visualizer(option)
+        visualizer.add('0 0 * * * /usr/bin/somecommand')
+        expect(visualizer.records.size).to eq(1)
+      end
+
+      it 'should add an extra' do
+        option = CronvRb::Option.new_cronv_option(now)
+        visualizer = CronvRb::Visualizer.new_visualizer(option)
+        visualizer.add('@reboot /usr/bin/somecommand')
+        expect(visualizer.extras.size).to eq(1)
+      end
+    end
+
+    context 'when the line is empty' do
+      it 'should not add a record' do
+        option = CronvRb::Option.new_cronv_option(now)
+        visualizer = CronvRb::Visualizer.new_visualizer(option)
+        visualizer.add('')
+        expect(visualizer.records.size).to eq(0)
+      end
+    end
+
+    context 'when the line starts with #' do
+      it 'should not add a record' do
+        option = CronvRb::Option.new_cronv_option(now)
+        visualizer = CronvRb::Visualizer.new_visualizer(option)
+        visualizer.add('# This is a comment')
+        expect(visualizer.records.size).to eq(0)
+      end
+    end
+  end
 end
