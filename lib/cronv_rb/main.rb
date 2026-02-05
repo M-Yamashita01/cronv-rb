e66d2e71c358a927da96009e6d2b4728cf6081d5
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2026-02-06 06:28:59 +0900

    feat: implement CLI main logic

--- lib/cronv_rb/main.rb
+++ lib/cronv_rb/main.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
 module CronvRb
   def self.main
-    Option.new_cronv_option(Time.now)
+    option = Option.new_cronv_option(Time.now)
+
+    OptionParser.new do |opt|
+      opt.on('-o', '--output PATH', 'path to .html file to output') { |v| option.output_file_path = v }
+      opt.on('-d', '--duration DURATION', 'duration to visualize in N{suffix} style. e.g.) 1d(day)/1h(hour)/1m(minute)') { |v| option.duration = v }
+      opt.on('--from-date DATE', "start date in the format '#{Option::OPT_DATE_FORMAT}' to visualize") { |v| option.from_date = v }
+      opt.on('--from-time TIME', "start time in the format '#{Option::OPT_TIME_FORMAT}' to visualize") { |v| option.from_time = v }
+      opt.on('-t', '--title TITLE', 'title/label of output') { |v| option.title = v }
+      opt.on('-w', '--width WIDTH', 'Table width of output') { |v| option.width = v.to_i }
+    end.parse!
+
+    visualizer = Visualizer.new_visualizer(option)
+
+    $stdin.each_line do |line|
+      visualizer.add(line)
+    end
+
+    path = visualizer.dump
+    puts "[#{option.title}] #{visualizer.records.size} tasks."
+    puts "[#{option.title}] '#{path}' generated."
   end
 end
